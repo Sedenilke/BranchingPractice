b9b0b9d7b03d34825647c54a946af63bcd6aa542
Sedenilke <sedenilke@gmail.com>
2022-12-19 20:48:58 -0800

    I added a new class and modified the main class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is the code from my main Project!");
         System.out.println("You can add more features in it!");
+        TestClass01 obj = new TestClass01();
+        obj.number1 =1;
+        obj.number2 =2;
+        obj.number3 =3;
+
     }
 }
---
+++ src/TestClass01.java
@@ -0,0 +1,5 @@
+public class TestClass01 {
+    int number1;
+    int number2;
+    int number3;
+}
